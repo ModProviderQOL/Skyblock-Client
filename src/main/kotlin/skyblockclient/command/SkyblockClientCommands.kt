dfb32972eb133b73cc133531858da56357fa7bb8
ModProviderQOL <salamentokina@gmail.com>
2025-05-04 10:11:43 +0300

    Add files via upload

---
+++ src/main/kotlin/skyblockclient/command/SkyblockClientCommands.kt
@@ -0,0 +1,48 @@
+package skyblockclient.command
+
+import net.minecraft.command.CommandBase
+import net.minecraft.command.ICommandSender
+import skyblockclient.SkyblockClient.Companion.config
+import skyblockclient.SkyblockClient.Companion.display
+import skyblockclient.config.Config.mimicMessage
+import skyblockclient.features.CapeManager
+import skyblockclient.utils.Utils.modMessage
+
+class SkyblockClientCommands : CommandBase() {
+    override fun getCommandName(): String {
+        return "sbclient"
+    }
+
+    override fun getCommandAliases(): List<String> {
+        return listOf(
+            "skyblockclient",
+            "sbc"
+        )
+    }
+
+    override fun getCommandUsage(sender: ICommandSender): String {
+        return "/$commandName"
+    }
+
+    override fun getRequiredPermissionLevel(): Int {
+        return 0
+    }
+
+    override fun processCommand(sender: ICommandSender, args: Array<String>) {
+        if (args.isEmpty()) {
+            display = config.gui()
+            return
+        }
+        when (args[0].lowercase()) {
+            "mimicmessage" -> {
+                args[0] = ""
+                val message = args.joinToString(" ").trim()
+                mimicMessage = message
+                modMessage("§aMimic message changed to §f$message")
+            }
+            "reloadcape" -> {
+                CapeManager.loadCape()
+            }
+        }
+    }
+}
